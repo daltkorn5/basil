87e826c9845b5f60054a4ac83e5b5fd5dbb57ca5
Danny Altkorn <daltkorn5@gmail.com>
2019-09-07 22:09:41 -0700

    added marshmallow schemas to ORMs

--- backend/src/tables/categories.py
+++ backend/src/tables/categories.py
@@ -2,11 +2,18 @@
 
 from sqlalchemy import Column, String, Integer
 from .table_base import Base, TableBase
+from marshmallow import Schema, fields
+
+class CategorySchema(Schema):
+    category_id = fields.Integer()
+    name = fields.String()
 
 class Category(TableBase, Base):
     __tablename__ = 'categories'
     category_id = Column(Integer, primary_key = True)
     name = Column(String)
+
+    schema = CategorySchema
 
     def __init__(self, name):
         self.name = name
--- backend/src/tables/transactions.py
+++ backend/src/tables/transactions.py
@@ -1,9 +1,19 @@
 # coding=utf-8
 
-from sqlalchemy import Column, Date, String, Integer, Numeric, ForeignKey
+from sqlalchemy import Column, Date, String, Integer, Numeric, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship
-from .table_base import Base, TableBase
+from .table_base import Base, TableBase, Session
 from .categories import Category
+from .description_category_xref import DescriptionCategoryXref
+from marshmallow import Schema, fields
+
+class TransactionSchema(Schema):
+    transaction_id = fields.Integer()
+    transaction_date = fields.Date()
+    amount = fields.String() # Ideally would be decimal but decimal type isn't serializable
+    category_id = fields.Integer()
+    description = fields.String()
+    created_by = fields.String()
 
 class Transaction(TableBase, Base):
     __tablename__ = 'transactions'
@@ -15,11 +25,23 @@ class Transaction(TableBase, Base):
     description = Column(String)
     created_by = Column(String)
 
-    categories = relationship('Category')
+    schema = TransactionSchema
 
-    def __init__(self, transaction_date, amount, category_id, description, created_by):
+    __table_args__ = (UniqueConstraint("transaction_date", "amount", "description"),)
+
+    def __init__(self, transaction_date, amount, description, created_by, category_id = None):
         self.transaction_date = transaction_date
         self.amount = amount
-        self.category_id = category_id
         self.description = description
         self.created_by = created_by
+        self.category_id = self.lookup_category(description) if category_id is None else category_id
+
+    def lookup_category(self, desc):
+        """
+        Check to see if we already have a mapping for the newly added transaction's description
+        """
+        xref = session.query(DescriptionCategoryXref).get(desc)
+        if xref is not None:
+            return xref.category_id
+
+        return None
