46540b6dd5553012dce6de838be9bc5c8c6c37ce
Danny Altkorn <daltkorn5@gmail.com>
2019-09-17 18:52:33 -0700

    added custon JSON encoder to keep dates in ISO format

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,13 +1,32 @@
-import os
+import os, datetime
 from flask import Flask
+from flask.json import JSONEncoder
 from .tables.table_base import engine, Base, Session
 from .tables.categories import Category
 from .tables.transactions import Transaction
 from .tables.description_category_xref import DescriptionCategoryXref
 
+class CustomJSONEncoder(JSONEncoder):
+    """
+    Class to make it so that flask's jsonify function keeps dates and datetimes in
+    iso format
+    taken from https://stackoverflow.com/questions/43663552/keep-a-datetime-date-in-yyyy-mm-dd-format-when-using-flasks-jsonify
+    """
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime.date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
 def create_app():
     Base.metadata.create_all(engine)
     app = Flask(__name__)
+    app.json_encoder = CustomJSONEncoder
 
     app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
